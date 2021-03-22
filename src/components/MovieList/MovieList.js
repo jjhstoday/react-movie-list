c3fb4a69f376397861c306214e953f84bf4b77d5
jjhstoday <jjh.s.today@gmail.com>
2021-03-22 20:36:29 +0900

    feat: Implement rating

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -3,11 +3,26 @@ import {
   moviesContainer,
   movieStyle,
   movieTitle,
-  movieYear,
-  movieSummary,
+  movieInfo,
+  movieRating,
+  purpleRating,
+  blackRating
 } from './MovieList.module.scss';
 
+const createRating = rating => {
+  const MAX = 5;
+  const movieRating = Math.round(rating / 2);
+
+  return (
+    <>
+      <span className={purpleRating}>{'★'.repeat(movieRating)}</span>
+      <span className={blackRating}>{'★'.repeat(MAX - movieRating)}</span>
+    </>
+  );
+};
+
 export default function MovieList({ movies }) {
+  console.log(movies);
   return (
     <>
       <ul className={moviesContainer}>
@@ -16,8 +31,11 @@ export default function MovieList({ movies }) {
             <li key={movie.id} className={movieStyle}>
               <img src={movie.small_cover_image} alt={movie.title} />
               <span className={movieTitle}>{movie.title}</span>
-              <span className={movieYear}>{movie.year}</span>
-              <span className={movieSummary}>{movie.summary}</span>
+              <span
+                className={movieInfo}
+              >{`${movie.year} | ${movie.genres}`}</span>
+              <span className={movieRating}>{createRating(movie.rating)}</span>
+              {/* <span className={movieSummary}>{movie.summary}</span> */}
             </li>
           </>
         ))}
